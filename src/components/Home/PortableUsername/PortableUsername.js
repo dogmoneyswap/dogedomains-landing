d453bf61871c244542ad8fcd846186296bd18826
Kasumi <kasumi.null@yandex.com>
2022-02-23 22:33:02 +0000

    feat: change name.eth -> .bch

--- src/components/Home/PortableUsername/PortableUsername.js
+++ src/components/Home/PortableUsername/PortableUsername.js
@@ -91,7 +91,7 @@ const Avatar = () => {
       </AvatarContainerInner>
       <AvatarContainerInner ref={containerRefTwo} style={{ opacity: 0 }}>
         <AvatarImg src={nickAvatar} />
-        <AvatarText>nick.eth</AvatarText>
+        <AvatarText>pat.bch</AvatarText>
       </AvatarContainerInner>
     </AvatarContainer>
   )
