d5c9a6a76f73e273c29336e2b4b689420df59c63
Kasumi <kasumi.null@yandex.com>
2022-02-23 22:13:19 +0000

    feat: remove claim banner on homepage

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -30,9 +30,6 @@ export default function Hero(props) {
   const { t } = useTranslation()
   return (
     <HeroContainer>
-      <MainPageBannerContainer>
-        <DAOBannerContent />
-      </MainPageBannerContainer>
       <Title>{t("home.hero.title")}</Title>
     </HeroContainer>
   )
