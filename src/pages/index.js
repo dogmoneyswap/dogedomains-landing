4554f9648ed4f91a3afb84b09a80dfcfb741a3b0
Kasumi <kasumi.null@yandex.com>
2022-02-27 18:19:32 +0000

    chore: remove statistics (for now)

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ export default function Home(props) {
       <Hero />
       <Carousel />
       <PortableUsername />
-      <Statistics />
       <Cryptocurrencies />
       <DecentralisedWebsites />
       <TraditionalDomains />
